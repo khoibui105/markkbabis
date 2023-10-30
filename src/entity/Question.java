27c422d17e2452ad901e6d99e435e301c5fa9f0f
khoibui105 <buiminhkhoi105@gmail.com>
2023-10-30 14:12:44 -0400

    question entity class

--- src/entity/Question.java
+++ src/entity/Question.java
@@ -1,4 +1,22 @@
 package entity;
 
+import java.util.List;
+
 public class Question {
+    private String question;
+    private String correctAnswer;
+    private List<String> inCorrectAnswer;
+
+    public Question(String question, String correctAnswer, List<String> inCorrectAnswer) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.inCorrectAnswer = inCorrectAnswer;
+    }
+
+    public String getAnswer() {
+        return correctAnswer;
+    }
+    public String getQuestion() {
+        return question;
+    }
 }
